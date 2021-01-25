f72725c80fcfac8a1a1bef8d1b2e937eaeb1d67c
Tashi Tsering Sherpa <sherpa.tash@gmail.com>
2021-01-25 14:36:57 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,38 @@
+// Tip Calculator
+
+function billAmount() {
+  var billAmt = document.getElementById("totalBill").value;
+  console.log("Bill Amt: " + billAmt);
+  return billAmt;
+  //document.getElementById("tipSpan").innerHTML = billAmt;
+}
+function tipPercent() {
+  var tipPercent1 = parseFloat(document.getElementById("selectQuality").value);
+  console.log(tipPercent1);
+  return tipPercent1;
+}
+
+function noOfPeople() {
+  var noOfPeople1 = parseFloat(document.getElementById("peopleamt").value);
+
+  if (isNaN(noOfPeople1)) {
+    noOfPeople1 = 1;
+  } else {
+    console.log("not nan");
+  }
+  console.log("No of People: " + noOfPeople1);
+  return noOfPeople1;
+}
+
+function tipCalculator() {
+  document.getElementById("tipSpan").innerHTML = parseFloat(
+    (billAmount() * tipPercent()) / noOfPeople()
+  );
+}
+
+function calculateTotal() {
+  tipCalculator();
+  billAmount();
+  tipPercent();
+  noOfPeople();
+}
